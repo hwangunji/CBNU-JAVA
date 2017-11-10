015a4681c482d839b612cb2b20af34205375eeb1
hwangunji <32187514+hwangunji@users.noreply.github.com>
2017-11-10 16:15:45 +0900

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
+import java.awt.*;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Terran marine= new Terran();
-		marine.attack();
-		Protoss dragoon = new Protoss();
-		dragoon.attack();
-		Zerg hydralisk=new Zerg();
-		hydralisk.attack();
-		System.out.println("====객체 형변환과 오버라이딩을 이용====");
-		Starcraft obj=new Terran();
-		
-		obj.attack();
-		obj=new Protoss();
-		obj.attack();
-		obj=new Zerg();
-		obj.attack();
-		obj=new Starcraft();
-		obj.shot();
+class Main
+{
+	 static void main(String args[])
+	{
+		FrameTest Obj=new FrameTest();		
 	}
-
 }
